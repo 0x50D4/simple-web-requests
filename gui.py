4790935a39a7c885d0d70784edf04284f2895d67
0x50D4 <sodasec@protonmail.com>
2023-07-14 21:52:30 +0200

    Added length, started implementing copy, added threading for the web request.

--- gui.py
+++ gui.py
@@ -2,9 +2,18 @@ import dearpygui.dearpygui as dpg
 import re
 import network 
 import tools
+import time
+import threading
 
 new_data = 1 # variable for tracking if we have new data
 domain = ""
+
+class default_values:
+    ww = 200
+    wh = 60
+
+dv = default_values()
+
 
 usr_input = default = "GET / HTTP/1.1\nHost: example.com\nConnection: close\n" # default request
 
@@ -31,9 +40,11 @@ def text_callback(sender):
     new_data = 1 # setting new data to 1, so we know we have nwe data
     usr_input = dpg.get_value(sender) # returning the value of the text box into usr_input
 
+def do_network(domain, usr_input):
+   dpg.configure_item("output", default_value=network.main(domain, usr_input)) 
+
 def btn_callback():
     """callback called upon hitting send"""
-
     global usr_input, domain, new_data
 
     if new_data: # run if we have new data, without this we would get a loop upon launching the same request
@@ -43,13 +54,17 @@ def btn_callback():
 
     print(repr(usr_input)) # print user input with \n and \r
     print(usr_input) # print user input(the request) formatted
-    dpg.configure_item("output", default_value=network.main(domain, usr_input)) # call the main netowrk function for the output textbox
+    x = threading.Thread(target=do_network, args=(domain, usr_input,)) # call the main netowrk function for the output textbox
+    x.start()
 
 def encode_popup():
     dpg.show_item("encode-popup")
 
 def decode_popup():
-    dpg.show_item("decode-popup") 
+    dpg.show_item("decode-popup")
+
+def length_popup():
+    dpg.show_item("length-popup")
 
 def encode_data(sender, value, userdata):
     input = userdata[0]
@@ -61,6 +76,10 @@ def decode_data(sender, value, userdata):
     output = userdata[1]
     dpg.set_value(output, tools.decode(dpg.get_value(input)))
 
+def length_data(sender, value, userdata):
+    input = userdata[0]
+    output = userdata[1]
+    dpg.set_value(output, tools.length(dpg.get_value(input)))
 
 def set_up_gui():
     dpg.create_context()
@@ -81,16 +100,26 @@ def set_up_gui():
         with dpg.menu(label="Tools"):
             dpg.add_menu_item(label="Encode", callback=encode_popup)
             dpg.add_menu_item(label="Decode", callback=decode_popup)
-    
-    with dpg.window(label="Encode", width=200, height=60, tag="encode-popup", show=False):
+            dpg.add_menu_item(label="Length", callback=length_popup)
+   
+
+    # encoding window
+    with dpg.window(label="Encode", width=dv.ww, height=dv.wh, tag="encode-popup", show=False):
         u_input = dpg.add_input_text(hint="Enter some encodable text") 
         output = dpg.add_input_text(readonly=True)
         dpg.add_button(label="Encode", callback=encode_data, user_data=[u_input, output])
-
-    with dpg.window(label="Decode", width=200, height=60, tag="decode-popup", show=False):
+    
+    # decoding window
+    with dpg.window(label="Decode", width=dv.ww, height=dv.wh, tag="decode-popup", show=False):
         u_input = dpg.add_input_text(hint="Enter some decodable text")
         output = dpg.add_input_text(readonly=True)
         dpg.add_button(label="Decode", callback=decode_data, user_data=[u_input, output])
+
+    # decoding window
+    with dpg.window(label="Length", width=dv.ww, height=dv.wh, tag="length-popup", show=False):
+        u_input = dpg.add_input_text(hint="Enter some text")
+        output = dpg.add_input_text(readonly=True)
+        dpg.add_button(label="Length", callback=length_data, user_data=[u_input, output])
 
     dpg.show_viewport()
     dpg.start_dearpygui()
